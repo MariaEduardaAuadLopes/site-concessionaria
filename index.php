275f1147af9bf97577bbbc9b873d59005fa1461d
MariaEduardaAuadLopes <maria.ealopes2@senacsp.edu.br>
2023-02-23 22:25:59 -0300

    feito

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concessionária</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="icone.png" alt="" width="5%"> Dud´sCar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRODUTOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CADASTRO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTATO</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="carrossedan.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="sedanvermelho.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="sedanprata.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item active">
+                <img src="minecupe.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item active">
+                <img src="carropreto.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="preto.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="carrinho.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="vermelha.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="azulao.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="verde.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="amarelo.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="dois.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="carrinhoo.png" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-success">Carro</h5>
+                        <p class="card-text">No nosso site você encontra os carros com os melhores modelos e preços.</p>
+                        <a href="#" class="btn btn-success btn-lg">Compre já</a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <h2 class="display-6 text-center">CADASTRO</h2>
+            </div>
+        </div>
+   
+    
+
+    <form class="row g-3">
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="inputNome">
+        </div>
+        <div class="col-md-6">
+            <label for="inputEmail" class="form-label">E-mail</label>
+            <input type="text" class="form-control" id="inputEmail">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress" class="form-label">Endereço</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="">
+        </div>
+        <div class="col-12">
+            <label for="inputAddress2" class="form-label">Cidade</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="">
+        </div>
+        
+
+
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                   Confirmar Cadastro
+                </label>
+            </div>
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
+        </div>
+        
+    </form>
+    </div>
+    </div>
+    </div>
+    
+
+
+
+
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <h2 class="display-6 text-center">CONTATO</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.328528678125!2d-47.33128268503708!3d-22.753186085090153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1677113258211!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+    </main>
+    <footer class="container bg-light">
+        <div class="row">
+            <div class="col">
+            <a class="navbar-brand" href="#"><img src="icone.png" alt="" width="5%"> Dud´sCar</a>
+                
+            </div>
+
+            <div class="col text-end">
+              <a href="#"> @Dud´sCar <img src="instagram.png" width="5%"></a>  
+            </div>
+
+
+        </div>
+    </footer>
+    <footer class="container bg-light">
+        <div class="row">
+            <div class="col">
+
+            </div>
+
+            <div class="col text-end">
+            <a href="#"> @Dud´sCar <img src="twitter.png" width="5%"></a> 
+            </div>
+
+
+
+        </div>
+    </footer>
+    <footer class="container bg-light">
+        <div class="row">
+            <div class="col">
+
+            </div>
+
+            <div class="col text-end">
+            <a href="#"> @Dud´sCar <img src="face.png" width="5%"></a> 
+            </div>
+
+
+
+        </div>
+    </footer>
+
+
+
+    </div>
+
+
+    <main class="container-fluid">
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
